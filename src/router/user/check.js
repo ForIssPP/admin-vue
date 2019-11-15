5cd61d23325ef7fcf18bd2b94f7175ad190c3956
siykt <cnsiykt@163.com>
2019-11-15 17:17:10 +0800

    Add registrar related routers(test).

--- src/router/user/check.js
+++ src/router/user/check.js
@@ -1,6 +1,6 @@
 import routerFactory from '@/utils/router-factory';
 
-const name = ['nameCheck', 'avatarCheck', 'profilesCheck', 'albumCheck', 'userPictureCheck', 'moneyCheck'];
+const name = ['nameCheck', 'avatarCheck', 'profilesCheck', 'albumCheck', 'userPictureCheck', 'moneyPictureCheck'];
 
 export default [
   ...routerFactory(
@@ -9,7 +9,9 @@ export default [
       () => import('@/views/users/name-check/index'),
       () => import('@/views/users/avatar-check/index'),
       () => import('@/views/users/profiles-check/index'),
-      ...Array(name.length - 3).fill(() => import('@/views/nested/menu1/menu1-1'))
+      () => import('@/views/users/album-check/index'),
+      () => import('@/views/users/user-picture-check/index'),
+      () => import('@/views/users/money-picture-check/index'),
     ],
     name,
     ['昵称审核', '头像审核', '个人介绍审核', '相册审核', '真人认证审核', '财产认证审核']
