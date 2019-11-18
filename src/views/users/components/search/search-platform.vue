d9b7e87cb5bee01345716c0a23bb54720294d54c
siykt <cnsiykt@163.com>
2019-11-18 13:10:07 +0800

    Add Search Components
    1. add serch vip component.
    2. add serch address component.
    3. add serch user create type component.
    4. add serch platform component.
    5. add serch number state component.

---
+++ src/views/users/components/search/search-platform.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-select
+    v-model="platformType"
+    placeholder="渠道"
+    clearable
+    @change="platformTypeChange"
+    class="filter-item"
+    style="width: 80px"
+  >
+    <el-option v-for="item in platformTypeOptions" :key="item" :label="item" :value="item" />
+  </el-select>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      platformType: "",
+      platformTypeOptions: ["iOS", "安卓"]
+    };
+  },
+  methods: {
+    platformTypeChange() {
+      this.$emit("platformTypeChange", this.platformType);
+    }
+  }
+};
+</script>
---
+++ src/views/users/components/search/search-user-create-type.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-select
+    v-model="userCreateTypeType"
+    placeholder="用户类型"
+    clearable
+    @change="userCreateTypeTypeChange"
+    class="filter-item"
+    style="width: 110px"
+  >
+    <el-option v-for="item in userCreateTypeTypeOptions" :key="item" :label="item" :value="item" />
+  </el-select>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userCreateTypeType: "",
+      userCreateTypeTypeOptions: ["后台", "手机端"]
+    };
+  },
+  methods: {
+    userCreateTypeTypeChange() {
+      this.$emit("userCreateTypeTypeChange", this.userCreateTypeType);
+    }
+  }
+};
+</script>
