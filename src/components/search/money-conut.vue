5d3d29e6d866cb7700b9517aaeb82a307509286c
siykt <cnsiykt@163.com>
2019-11-20 14:30:12 +0800

    Add Search Components
    1. add money conut component.
    2. add search state finance component.

---
+++ src/components/search/money-conut.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="filter-item count-to">
+    <el-col :span="13">{{ name }}</el-col>
+    <el-col :span="11">
+      <count-to :start-val="0" :end-val="money" :duration="3000" :prefix="'¥'"></count-to>
+    </el-col>
+  </div>
+</template>
+<script>
+import countTo from "vue-count-to";
+
+export default {
+  components: { countTo },
+  props: {
+    money: {
+      type: Number,
+      required: true
+    },
+    name: {
+      type: String,
+      default: "总金额统计"
+    }
+  }
+};
+</script>
+<style lang="scss">
+.count-to {
+  padding: 0 10px;
+  margin-right: 10px;
+  width: 200px;
+  color: #1890ff;
+  font-size: 14px;
+  line-height: 36px;
+  border-radius: 4px;
+  border: 1px solid #dcdfe6;
+
+  div:first-child {
+    color: #606266;
+  }
+}
+</style>
---
+++ src/components/search/search-state-finance.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-select
+    v-model="payStateType"
+    placeholder="账号状态"
+    clearable
+    @change="payStateTypeChange"
+    class="filter-item"
+    style="width: 110px"
+  >
+    <el-option v-for="item in payStateTypeOptions" :key="item" :label="item" :value="item" />
+  </el-select>
+</template>
+<script>
+import { getPayList } from "@/api/get-config";
+
+export default {
+  data() {
+    return {
+      payStateType: "",
+      payStateTypeOptions: []
+    };
+  },
+  created() {
+    getPayList().then(
+      response => (this.payStateTypeOptions = response.data.items)
+    );
+  },
+  methods: {
+    payStateTypeChange() {
+      this.$emit("payStateTypeChange", this.payStateType);
+    }
+  }
+};
+</script>
