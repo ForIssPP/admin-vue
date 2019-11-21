827a87ad58d3c5d9272b2a6e397b53f96e602725
siykt <cnsiykt@163.com>
2019-11-21 20:17:29 +0800

    Add userCommonOpenConfirm Function

--- src/utils/open-confirm.js
+++ src/utils/open-confirm.js
@@ -2,7 +2,7 @@
  * 打开默认confirm
  * @param {function} callback 回调函数
  */
-export function _confirm(callback) {
+export function commonConfirm(callback) {
   if (this && this.$confirm) {
     return this.$confirm("此操作将会修改状态，是否继续?", "提示", {
       confirmButtonText: "确定",
@@ -18,5 +18,34 @@ export function _confirm(callback) {
         callback();
       })
       .catch(() => undefined);
+  }
+}
+
+export function userCommonOpenConfirm(tag, row) {
+  if (this && this.$confirm) {
+    const openConfirm = commonConfirm.bind(this);
+    const handleUpdate = () => {
+      row.state = "待审核";
+    };
+
+    const handlePass = () => {
+      row.state = "通过";
+    };
+
+    const handleNotPass = () => {
+      row.state = "未通过";
+    };
+
+    if (tag === "update") {
+      openConfirm(handleUpdate);
+    }
+
+    if (tag === true) {
+      openConfirm(handlePass);
+    }
+
+    if (tag === false) {
+      openConfirm(handleNotPass);
+    }
   }
 }
