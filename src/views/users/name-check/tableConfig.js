a44e992e814343f9d6444d1489fc7485792f82fb
siykt <cnsiykt@163.com>
2019-11-21 16:23:00 +0800

    Feat name-check page simple features.

---
+++ src/views/users/name-check/tableConfig.js
@@ -0,0 +1,30 @@
+export const tableHeader = [
+  "ID",
+  "电话号码",
+  "状态",
+  "性别",
+  "审核昵称",
+  "修改时间",
+  "处理人"
+];
+
+export const tableContent = [
+  "id",
+  "phoneNumber",
+  "state",
+  "sex",
+  "checkName",
+  "time",
+  "reviewer"
+];
+
+export const componentList = [
+  "TableId",
+  "TablePhoneNumber",
+  "TableSex",
+  "TableState",
+  "TableCheckName",
+  "TableTime",
+  "TableReviewer",
+  "TableChoise"
+];
