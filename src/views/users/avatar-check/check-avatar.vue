81df146197c3a79075de3de14004ba1ec61ee3b7
siykt <cnsiykt@163.com>
2019-11-21 20:18:43 +0800

    Feat avatar-check page simple features.

---
+++ src/views/users/avatar-check/check-avatar.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Check Avatar -->
+  <el-table-column label="待审头像" align="center">
+    <template slot-scope="{row}">
+      <img :src="row.checkAvatar" alt="check-avatar" />
+    </template>
+  </el-table-column>
+</template>
+<script>
+export default {
+  name: "table-check-avatar"
+};
+</script>
---
+++ src/views/users/avatar-check/table-config.js
@@ -0,0 +1,14 @@
+import checkAvatar from "./check-avatar.vue";
+import oldAvatar from "./old-avatar.vue";
+
+export const componentList = [
+  "TableId",
+  "TablePhoneNumber",
+  "TableSex",
+  "TableState",
+  checkAvatar,
+  oldAvatar,
+  "TableTime",
+  "TableReviewer",
+  "TableChoise"
+];
