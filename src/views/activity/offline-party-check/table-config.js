a15e8da64e7ecc0eb6a62a6d0865aa821329cd75
siykt <cnsiykt@163.com>
2019-12-02 17:10:22 +0800

    Feat offline-party-check page simple features.

---
+++ src/views/activity/offline-party-check/table-config.js
@@ -0,0 +1,15 @@
+import picture from "./picture.vue";
+
+export const componentList = [
+  "TableId",
+  "TableUsername",
+  "TableAddress",
+  "TableSex",
+  "TablePlatform",
+  "TableTitle",
+  picture,
+  "TableStateActivity",
+  "TableTime",
+  "TableReviewer",
+  "TableChoiseActivity",
+];
