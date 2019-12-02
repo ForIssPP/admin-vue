6a375cf00a7bf007112a0acab483025d627358c7
siykt <cnsiykt@163.com>
2019-12-02 15:50:15 +0800

    Feat add defendant table component

--- src/views/message/tip-offs/table-config.js
+++ src/views/message/tip-offs/table-config.js
@@ -1,8 +1,10 @@
 import pictures from "./pictures.vue";
+import defendant from './defendant.vue'
 
 export const componentList = [
   "TableId",
   "TableUsername",
+  defendant,
   "TableVip",
   "TableTipOffType",
   "TableTipOffState",
