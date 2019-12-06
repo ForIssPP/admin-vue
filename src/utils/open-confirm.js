2db92712c79a288e119b9e191de8cc0b3e668cc6
siykt <cnsiykt@163.com>
2019-12-06 14:48:15 +0800

    Add Notification, MessageBox Components

--- src/utils/open-confirm.js
+++ src/utils/open-confirm.js
@@ -1,59 +1,130 @@
+import { Notification, MessageBox } from "element-ui";
+
+function handleSuccess() {
+  Notification({
+    type: "success",
+    message: "修改成功!"
+  });
+}
+
 /**
  * 打开默认confirm
  * @param {function} callback 回调函数
  */
 export function commonConfirm(callback) {
-  if (this && this.$confirm) {
-    return this.$confirm("此操作将会修改状态，是否继续?", "提示", {
-      confirmButtonText: "确定",
-      cancelButtonText: "取消",
-      lockScroll: false,
-      type: "warning"
-    })
-      .then(callback)
-      .catch(() => undefined);
+  return MessageBox.confirm("此操作将会修改状态，是否继续?", "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    lockScroll: false,
+    type: "warning"
+  })
+    .then(callback)
+    .catch(err => console.log(err));
+}
+
+/**
+ * User 默认操作
+ * @param {string} tag 类型
+ * @param {object} row 列表对象
+ * @param {function} handleSetState 修改操作函数
+ */
+export function userCommonOpenConfirm(tag, row, handleSetState) {
+  const handleUpdate = () => {
+    row.state = "0";
+  };
+
+  const handlePass = () => {
+    handleSetState(row).then(res => {
+      handleSuccess();
+      row.state = "1";
+    });
+  };
+
+  const handleNotPass = () => {
+    handleSetState(row).then(res => {
+      handleSuccess();
+      row.state = "2";
+    });
+  };
+
+  if (tag === "update") {
+    commonConfirm(handleUpdate);
+  }
+
+  if (tag === true) {
+    commonConfirm(handlePass);
+  }
+
+  if (tag === false) {
+    commonConfirm(handleNotPass);
   }
 }
 
-export function userCommonOpenConfirm(tag, row, handleSetState) {
-  if (this && this.$confirm) {
-    const openConfirm = commonConfirm.bind(this);
+/**
+ * Activity 默认操作
+ * @param {string} tag 类型
+ * @param {object} row 列表对象
+ * @param {function} handleSetState 修改操作函数
+ */
+export function activityCommonOpenConfirm(tag, row, handleSetState) {
+  const handlePass = () => {
+    handleSetState(row).then(res => {
+      handleSuccess();
+      row.check_state = "1";
+    });
+  };
 
-    const handleSuccess = () => {
-      this.$notify({
-        type: "success",
-        message: "修改成功!"
-      });
-    };
+  const handleNotPass = () => {
+    handleSetState(row).then(res => {
+      handleSuccess();
+      row.check_state = "0";
+    });
+  };
 
-    const handleUpdate = () => {
-      row.state = "0";
-    };
+  if (tag === true) {
+    commonConfirm(handlePass);
+  }
 
-    const handlePass = () => {
-      handleSetState(row).then(res => {
+  if (tag === false) {
+    commonConfirm(handleNotPass);
+  }
+}
+
+/**
+ * Finance 提现操作
+ * @param {string} tag 类型
+ * @param {object} row 列表对象
+ */
+export function financeCommonOpenConfirm(tag, row) {
+  const handlePass = () => {
+    import("@/api/finance").then(api =>
+      api.cashoutRemit(row).then(res => {
         handleSuccess();
         row.state = "1";
-      });
-    };
+      })
+    );
+  };
 
-    const handleNotPass = () => {
-      handleSetState(row).then(res => {
-        handleSuccess();
-        row.state = "2";
-      });
-    };
+  const handleNotPass = () => {
+    MessageBox.prompt("请输入驳回消息", "提示", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消"
+    }).then(({ value: msg }) => {
+      if (!msg) {
+        return Promise.reject(Error("请输入驳回信息"));
+      }
+      row.msg = msg;
+      import("@/api/finance").then(api =>
+        api.cashoutReject(row).then(res => (row.state = "1"))
+      );
+    });
+  };
 
-    if (tag === "update") {
-      openConfirm(handleUpdate);
-    }
+  if (tag === true) {
+    commonConfirm(handlePass);
+  }
 
-    if (tag === true) {
-      openConfirm(handlePass);
-    }
-
-    if (tag === false) {
-      openConfirm(handleNotPass);
-    }
+  if (tag === false) {
+    commonConfirm(handleNotPass);
   }
 }
