36f7445c3e1b588c349be88427f344b740249021
siykt <cnsiykt@163.com>
2019-12-06 16:06:51 +0800

    Add Pro Api

--- src/api/finance.js
+++ src/api/finance.js
@@ -1,25 +1,66 @@
-import request from "@/utils/request";
+import richpapaRequest from "@/utils/richpapa-request";
 
-export function getWithdrawList(data) {
-  return request({
-    url: "/finance/withdraw/list",
+export function getWithdrawList(query) {
+  query.service = "Finance.CashoutList";
+  return richpapaRequest({
     method: "get",
-    data
+    params: query
   });
 }
 
-export function getRechargeRecordList(data) {
-  return request({
-    url: "/finance/record/rechargelist",
+export function getRechargeRecordList(query) {
+  query.service = "Finance.RechargeList";
+  return richpapaRequest({
     method: "get",
-    data
+    params: query
   });
 }
 
-export function getPurchaseRecordList(data) {
-  return request({
-    url: "/finance/record/purchaselist/",
+export function getPurchaseRecordList(query) {
+  query.service = "Finance.ConsumeList";
+  return richpapaRequest({
     method: "get",
-    data
+    params: query
+  });
+}
+
+/**
+ * 提现驳回
+ * @param {object} row 列表对象
+ */
+export function cashoutReject(row) {
+  return richpapaRequest({
+    method: "get",
+    params: {
+      service: "Finance.CashoutReject",
+      cashout_id: row.uid,
+      reason: row.msg
+    }
+  });
+}
+
+/**
+ * 提现打款
+ * @param {object} row 列表对象
+ */
+export function cashoutRemit(row) {
+  return richpapaRequest({
+    method: "get",
+    params: {
+      service: "Finance.CashoutRemit",
+      cashout_id: row.uid
+    }
+  });
+}
+
+/**
+ * 手动充值
+ * @param {objeck} form 表单信息
+ */
+export function addUserAmount(form) {
+  form.service = "Finance.AddUserAmount";
+  return richpapaRequest({
+    method: "get",
+    params: form
   });
 }
