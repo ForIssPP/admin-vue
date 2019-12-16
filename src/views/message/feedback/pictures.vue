5632052978bf8adabb0d2a2c4a2ac01d5720f59d
siykt <cnsiykt@163.com>
2019-12-16 15:54:38 +0800

    Style add img width

--- src/views/message/feedback/pictures.vue
+++ src/views/message/feedback/pictures.vue
@@ -2,7 +2,13 @@
   <!-- Picture Check -->
   <el-table-column label="图片" prop="img" align="center">
     <template slot-scope="{row}">
-      <img width="100%" v-for="(src, index) in row.img" :key="index" :src="src" alt="tip-off-images" />
+      <img
+        width="50px"
+        v-for="(src, index) in row.img"
+        :key="index"
+        :src="src"
+        alt="tip-off-images"
+      />
     </template>
   </el-table-column>
 </template>
--- src/views/message/tip-offs/pictures.vue
+++ src/views/message/tip-offs/pictures.vue
@@ -2,7 +2,7 @@
   <!-- Picture Check -->
   <el-table-column label="图片" prop="tipOffImages" align="center">
     <template slot-scope="{row}">
-      <img width="100%" v-for="(src, index) in row.img" :key="index" :src="src" alt="tip-off-images" />
+      <img width="50px" v-for="(src, index) in row.img" :key="index" :src="src" alt="tip-off-images" />
     </template>
   </el-table-column>
 </template>
