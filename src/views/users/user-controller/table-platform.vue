c5614c5d7126912785ccc7421cb5f024822021a1
siykt <cnsiykt@163.com>
2019-12-17 11:28:04 +0800

    Add table Componenets

---
+++ src/views/users/user-controller/table-platform.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-table-column label="手机类型" prop="platform" :sortable="sortable" align="center" :width="width">
+    <template slot-scope="{row}">
+      <span>{{
+        row.device === '0' ? '没有设备' : row.device === '1' ? '安卓' : 'iOS'
+      }}</span>
+    </template>
+  </el-table-column>
+</template>
+<script>
+export default {
+  name: "table-platform",
+  props: {
+    width: {
+      type: String,
+      default: "80"
+    },
+    sortable: {
+      type: [Boolean, String],
+      validator(value) {
+        return typeof value === "boolean" || value === "custom ";
+      },
+      default: false
+    }
+  }
+};
+</script>
---
+++ src/views/users/user-controller/table-time.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-table-column label="注册时间" prop="time" :sortable="sortable" align="center" :width="width">
+    <template slot-scope="{row}">
+      <span>{{ new Date(Number(row.create_time)).toLocaleDateString() }}</span>
+    </template>
+  </el-table-column>
+</template>
+<script>
+export default {
+  name: "table-time",
+  props: {
+    width: {
+      type: String
+    },
+    sortable: {
+      type: [Boolean, String],
+      validator(value) {
+        return typeof value === "boolean" || value === "custom ";
+      },
+      default: false
+    }
+  }
+};
+</script>
